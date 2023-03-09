6d5f68523f3f97e07088b10224bfc12601b02034
Trqhxrd <trqhxrd@duck.com>
2023-03-09 09:43:40 +0100

    Fixed highlighting on home button.

---
+++ site/scripts/home.js
@@ -0,0 +1,15 @@
+function onmouseover() {
+    document.getElementById('icon-image').src='../../../assets/images/furnace_on.png';
+    document.getElementById('icon-text').style.color = 'white'
+    document.getElementById('icon-text').style.textDecoration = 'underline white'
+}
+
+function onmouseout() {
+    document.getElementById('icon-image').src='../../../assets/images/furnace_off.png';
+    document.getElementById('icon-text').style.color = '#dddddd'
+    document.getElementById('icon-text').style.textDecoration = 'none'
+}
+
+const btn = document.getElementById("icon-button")
+btn.onmouseover = onmouseover
+btn.onmouseout = onmouseout
