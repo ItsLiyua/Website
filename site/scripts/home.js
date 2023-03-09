f901b5384db841b6a418b974ab1dd46886522cf2
Trqhxrd <trqhxrd@duck.com>
2023-03-09 10:14:07 +0100

    home.js now looks hot.

--- site/scripts/home.js
+++ site/scripts/home.js
@@ -1,15 +1,15 @@
-function onmouseover() {
-    document.getElementById('icon-image').src = '../../../assets/images/furnace_on.png';
-    document.getElementById('icon-text').style.color = 'white'
-    document.getElementById('icon-text').style.textDecoration = 'underline white'
-}
-
-function onmouseout() {
-    document.getElementById('icon-image').src = '../../../assets/images/furnace_off.png';
-    document.getElementById('icon-text').style.color = '#dddddd'
-    document.getElementById('icon-text').style.textDecoration = 'none'
-}
-
 const btn = document.getElementById("icon-button")
-btn.onmouseover = onmouseover
-btn.onmouseout = onmouseout
+const img = document.getElementById('icon-image')
+const text = document.getElementById('icon-text')
+
+btn.onmouseover = () => {
+    img.src = '../../../assets/images/furnace_on.png';
+    text.style.color = 'white'
+    text.style.textDecoration = 'underline white'
+}
+
+btn.onmouseout = () => {
+    img.src = '../../../assets/images/furnace_off.png';
+    text.style.color = '#dddddd'
+    text.style.textDecoration = 'none'
+}
