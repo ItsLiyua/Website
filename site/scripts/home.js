32031a84d4b4fab16d72214b7e7535e8724ab550
Trqhxrd <trqhxrd@duck.com>
2023-03-09 09:53:24 +0100

    Reformatted all files.

--- site/scripts/home.js
+++ site/scripts/home.js
@@ -1,11 +1,11 @@
 function onmouseover() {
-    document.getElementById('icon-image').src='../../../assets/images/furnace_on.png';
+    document.getElementById('icon-image').src = '../../../assets/images/furnace_on.png';
     document.getElementById('icon-text').style.color = 'white'
     document.getElementById('icon-text').style.textDecoration = 'underline white'
 }
 
 function onmouseout() {
-    document.getElementById('icon-image').src='../../../assets/images/furnace_off.png';
+    document.getElementById('icon-image').src = '../../../assets/images/furnace_off.png';
     document.getElementById('icon-text').style.color = '#dddddd'
     document.getElementById('icon-text').style.textDecoration = 'none'
 }
