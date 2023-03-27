aa9d2481ce75fb1a7e9d36c77d05087781066e9b
Trqhxrd <trqhxrd@duck.com>
2023-03-27 14:11:54 +0200

    Added light-dark-mode to content pages

--- site/scripts/lightdarkmode.js
+++ site/scripts/lightdarkmode.js
@@ -1,17 +1,18 @@
 import {LDMS_MODE_DARK, LDMS_MODE_LIGHT, LIGHT_DARK_MODE_STATE} from "./constants.js";
-import {getCookie} from "./cookie-util.js";
+import {getCookie, setCookie} from "./cookie-util.js";
 
 const CSS_ROOT = document.querySelector(":root").style
 
 export function light() {
     sessionStorage.setItem(LIGHT_DARK_MODE_STATE, LDMS_MODE_LIGHT)
+    setCookie(LIGHT_DARK_MODE_STATE, LDMS_MODE_LIGHT)
     CSS_ROOT.setProperty("--bg", "var(--bg-light)")
     CSS_ROOT.setProperty("--accent", "var(--accent-light)")
     CSS_ROOT.setProperty("--accent-opaque", "var(--accent-opaque-light)")
 }
-
 export function dark() {
     sessionStorage.setItem(LIGHT_DARK_MODE_STATE, LDMS_MODE_DARK)
+    setCookie(LIGHT_DARK_MODE_STATE, LDMS_MODE_DARK)
     CSS_ROOT.setProperty("--bg", "var(--bg-dark)")
     CSS_ROOT.setProperty("--accent", "var(--accent-dark)")
     CSS_ROOT.setProperty("--accent-opaque", "var(--accent-opaque-dark)")
