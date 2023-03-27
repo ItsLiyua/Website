9ba6b6b7c12cfd401f2cc52dc7e9f8fea936f166
Trqhxrd <trqhxrd@duck.com>
2023-03-27 15:18:30 +0200

    Updated run.bat

--- run.bat
+++ run.bat
@@ -4,11 +4,14 @@ docker build -t website .
 echo Starting website.
 docker run --rm -d --name website -p 8080:80 website:latest
 
-echo Opening firefox.
-start /B firefox http://localhost:8080/Website/index.html
-
-pause
+echo.
+echo Open a Webbrowser and go to localhost:8080/Website/index.html
+echo Press any key to stop the server.
+echo.
+pause >nul
 
 echo Stopping website.
-docker stop website > /dev/null
+docker container stop website
 echo Stopped website.
+
+timeout /t 5
